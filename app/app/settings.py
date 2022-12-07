5d9cab22b47eb47d71c83bfd5a1ef639145bf01b
ivan <shvalov.i@gmail.com>
2022-12-07 12:59:14 +0700

    fix static

--- app/app/settings.py
+++ app/app/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 
 # Default primary key field type
