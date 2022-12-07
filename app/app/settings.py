fd498379f3fcf9adc829822e3ea3a6c842c29ab3
ivan <shvalov.i@gmail.com>
2022-12-07 12:52:49 +0700

    f

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', os.getenv('SERVER_IP', default=None)]
+ALLOWED_HOSTS = ['127.0.0.1', os.getenv('SERVER_IP', default=None)]
 
 
 # Application definition
