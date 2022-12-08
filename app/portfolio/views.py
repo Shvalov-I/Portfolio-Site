66a3f1766b886fa30105abf9ea0c2c9690754b7b
ivan <shvalov.i@gmail.com>
2022-12-08 19:06:18 +0700

    fix division of skills

--- app/portfolio/views.py
+++ app/portfolio/views.py
@@ -9,8 +9,8 @@ def index_view(request):
     skills = SkillsBar.objects.all()
     site_info = SiteInfo.objects.get(main_page=True)
     # Делим навыки на две части, чтобы разделить их на две соседние таблицы
-    first_skills = skills[:math.ceil(len(skills))]
-    second_skills = skills[math.ceil(len(skills)) + 1::]
+    first_skills = skills[:math.ceil(len(skills) / 2)]
+    second_skills = skills[math.ceil(len(skills) / 2) + 1::]
     return render(request, 'index.html', {'all_project': all_project, 'filters': filters, 'first_skills': first_skills,
                                           'second_skills': second_skills, 'info': site_info, })
 
